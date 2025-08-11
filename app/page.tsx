cc8cce423937f784e279e3e53f2ac394712049fc
yuuki008 <s22ah261@gmail.com>
2025-08-11 16:34:51 +0900

    Add logic to retrieve user posts and display a message when no posts are found

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
+import { getUserPosts } from "@/lib/database";
+import { createClient } from "@/lib/supabase/server";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient(cookies());
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  const posts = await getUserPosts(user?.id ?? "");
+
+  if (posts.length === 0) {
+    return <div>No posts found</div>;
+  }
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
