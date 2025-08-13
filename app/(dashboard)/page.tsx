5c9a91721eadb94f864a64d634924503212ffcbd
yuuki008 <s22ah261@gmail.com>
2025-08-13 09:52:45 +0900

    Add new post button with Plus icon and update text for no posts found message

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { getUserPosts } from "@/lib/database";
 import { createClient } from "@/lib/supabase/server";
+import { Plus } from "lucide-react";
 import { cookies } from "next/headers";
 
 export default async function Home() {
@@ -10,15 +12,23 @@ export default async function Home() {
 
   const posts = await getUserPosts(user?.id ?? "");
 
-  if (posts.length === 0) {
-    return <div className="text-center text-2xl">No posts found</div>;
-  }
-
   return (
     <div className="flex flex-col gap-4">
-      {posts.map((post) => (
-        <div key={post.id}>{post.content}</div>
-      ))}
+      {posts.length === 0 ? (
+        <div className="text-center text-2xl">No posts yet</div>
+      ) : (
+        <div className="flex flex-col gap-4">
+          {posts.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
+      )}
+      <div className="fixed w-full bottom-4 left-0 flex justify-center">
+        <Button className="rounded-full">
+          <Plus />
+          New Post
+        </Button>
+      </div>
     </div>
   );
 }
