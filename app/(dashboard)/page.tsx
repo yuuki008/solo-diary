c72fdf9bcee582d6c46d614a43ee8a7b0cf5bd42
yuuki008 <s22ah261@gmail.com>
2025-08-17 11:19:50 +0900

    Update Settings button in Home component to include title attribute

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -15,7 +15,12 @@ export default function Home() {
       <div className="fixed w-full left-0 bottom-4 z-20 flex justify-center">
         <div className="flex justify-center border rounded-full bg-background/80">
           <CreatePosterDrawer />
-          <Button className="rounded-full" size="icon" variant="ghost">
+          <Button
+            title="Settings"
+            className="rounded-full"
+            size="icon"
+            variant="ghost"
+          >
             <Settings className="!w-5 !h-5" />
           </Button>
         </div>
