db965e1165c5344ba2b149f0db99958ed4ee5c24
yuuki008 <s22ah261@gmail.com>
2025-08-23 18:27:02 +0900

    Update post card component layout and add created_at timestamp using formatDatetime function

--- app/(dashboard)/_components/post-card.tsx
+++ app/(dashboard)/_components/post-card.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import AttachmentsCarousel from "@/components/attachments-carousel";
+import { formatDatetime } from "@/lib/utils";
 import { PostWithAttachments } from "@/types/database";
 
 export default function PostCard({ post }: { post: PostWithAttachments }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col border-b pb-4">
       <AttachmentsCarousel attachments={post.post_attachments} />
-      <div className="text-sm overflow-wrap break-words mt-2">
+      <div className="text-sm overflow-wrap break-words mt-4">
         {post.content}
+      </div>
+      <div className="text-xs text-muted-foreground ml-auto mt-1">
+        {formatDatetime(post.created_at)}
       </div>
     </div>
   );
