7d134cf862df1e3313af80f9e41b33b94bebd7d1
yuuki008 <s22ah261@gmail.com>
2025-08-30 11:41:21 +0900

    Refactor Playground component UI for menu toggle button

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,9 +1,6 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
-import { Plus, X } from "lucide-react";
+import { Plus } from "lucide-react";
 import { useState } from "react";
 
 export default function Playground() {
@@ -11,43 +8,26 @@ export default function Playground() {
 
   return (
     <div className="relative w-full flex items-center justify-center">
-      <Button
-        className={cn(
-          "transition-opacity duration-300 ease-in-out",
-          isOpen ? "opacity-0" : "opacity-100"
-        )}
-        variant="ghost"
-        size="icon"
-        onClick={() => setIsOpen(true)}
-        aria-label="open menu"
-      >
-        <Plus className="w-4 h-4" />
-      </Button>
-
-      <div
-        className={cn(
-          "w-full max-w-[500px] mx-auto grid transition-all duration-800",
-          isOpen
-            ? "grid-rows-[1fr] w-full opacity-100"
-            : "grid-rows-[0fr] w-0 opacity-0"
-        )}
-      >
-        <div className="overflow-hidden border rounded-lg">
-          <div className="p-4 relative">
-            <Button
-              variant="ghost"
-              size="icon"
-              className="absolute top-2 right-2"
-              onClick={() => setIsOpen(false)}
-            >
-              <X className="w-4 h-4" />
-            </Button>
-
-            <div className="text-sm text-muted-foreground">New Post</div>
-
-            <Textarea />
-            <Button>Post</Button>
-          </div>
+      <div className="fixed bottom-2 w-full">
+        <div className="border relative w-fit mx-auto flex flex-col items-center max-w-[500px] rounded-lg">
+          <div
+            className={cn(
+              "transition-all duration-500",
+              isOpen ? "w-[500px] h-[500px]" : "w-0 h-0"
+            )}
+          ></div>
+          <button
+            onClick={() => setIsOpen((prev) => !prev)}
+            aria-label="open menu"
+            className="bg-transparent border-none cursor-pointer p-2"
+          >
+            <Plus
+              className={cn(
+                "w-6 h-6 transition-all duration-500",
+                isOpen ? "rotate-45" : "rotate-0"
+              )}
+            />
+          </button>
         </div>
       </div>
     </div>
